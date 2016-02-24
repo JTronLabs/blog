0e18ea79b237d9a773880fad149224348dc5d125
jtronlabs <jtronlabs@gmail.com>
2016-02-24 16:47:41 -0500

    Removed nav at top, only buttons now. If you collapse all then it is easy enough to see the nav via the sections. Also improved the nav buttons' interactivity. Now when you click one it resets with respect to the other.

--- js/main.js
+++ js/main.js
@@ -7,18 +7,22 @@ $( document ).ready(function() {
   $('#collapse-all-btn').on( "click", collapseAllHandler );
   $('#collapse-p-btn').on( "click", collapseParagraphHandler );
 
-  $('.collapsable').on( "click", expandOneCollapsable );
+  $('.collapsable').on( "click", expandCollapsableOnClick );
 });
 
 function collapseAllHandler(){
+  if(p_collapsed){ collapseParagraphHandler(); }//need to reset with respect to paragraphs
+
   if(all_collapsed){ //expand
     $('.collapsable').removeClass(" collapsed ");
     $('#collapse-all-btn').text('Collapse All');
+    $('#collapse-all-btn').prop('title', 'Hide all content');
 
     $('.collapsable-content').show();
   }else{ //collapse
     $('.collapsable').addClass(" collapsed ");
     $('#collapse-all-btn').text('Expand All');
+    $('#collapse-all-btn').prop('title', 'Show all content');
 
 
     $('.collapsable-content').hide();
@@ -27,13 +31,17 @@ function collapseAllHandler(){
 }
 
 function collapseParagraphHandler(){
+  if(all_collapsed){ collapseAllHandler(); }//need to reset with respect to all content
+
   if(p_collapsed){ //expand
     $('#collapse-p-btn').text('Advanced Users');
+    $('#collapse-p-btn').prop('title', 'Hide paragraphs');
 
     $('p').show();
 
   }else{ //collapse
     $('#collapse-p-btn').text('Beginner Users');
+    $('#collapse-p-btn').prop('title', 'Show paragraphs');
 
     //Using markdown, <img> tags are auto wrapped in <p> tags. I want to show images though. Thus do not hide the <p> if there is an image within it
     $('p').not(':has(img)').hide();
@@ -42,8 +50,8 @@ function collapseParagraphHandler(){
   p_collapsed = !p_collapsed;
 }
 
-function expandOneCollapsable(){
+function expandCollapsableOnClick(){
   $(this).removeClass("collapsed");
-  
+
   $(this).find(".collapsable-content").show();
 }
